fd46a16d812970367cc3d9e043492bb4b00ed01a
khunrebirth <khunrebirth@gmail.com>
2017-12-09 21:44:00 +0700

    fix: app.js

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame() {
             this.gameIsRunning = true
             this.playerHealth = 100
             this.monsterHealth = 100
+            this.turns = []
         },
         attack() {
-            this.monsterHealth -= this.calculateDamage(3, 10)
+            var damage = this.calculateDamage(3, 10)
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            })
             if (this.checkWin()) return
 
             this.monsterAttacks()
         },
         specialAttack() {
-            this.monsterHealth -= this.calculateDamage(10, 20)
+            var damage = this.calculateDamage(10, 20)
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            })
             if (this.checkWin()) return
 
             this.monsterAttacks()
@@ -29,13 +41,23 @@ new Vue({
             else
                 this.playerHealth = 100
 
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heal hard for ' + 10
+            })
+
             this.monsterAttacks()
         },
         giveUp() {
-
+            this.gameIsRunning = false
         },
         monsterAttacks() {
-            this.playerHealth -= this.calculateDamage(5, 12)
+            var damage = this.calculateDamage(3, 10)
+            this.playerHealth -= damage
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            })
             this.checkWin()
         },
         calculateDamage(min, max) {
